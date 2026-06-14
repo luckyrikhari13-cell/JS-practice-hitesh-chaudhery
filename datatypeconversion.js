83499e8e2470d6226ce1937d5f9db97ce33c2357
luckyrikhari13-cell <luckyrikhari13@gmail.com>
2026-06-14 19:33:58 +0530

    learned datatype conversion

---
+++ datatypeconversion.js
@@ -0,0 +1,29 @@
+let score = true 
+console.log(typeof score) // boolean
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber) // number
+console.log(valueInNumber) // 1
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1   
+// false => 0
+// null => 0
+// undefined => NaN
+
+let isLoggedIn = "lucky"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn) // true  
+
+// 1 => true
+// 0 => false
+// "" => false
+// "lucky" => true
+// null => false
+// undefined => false   
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber) // "33"
+console.log(typeof stringNumber) // string
