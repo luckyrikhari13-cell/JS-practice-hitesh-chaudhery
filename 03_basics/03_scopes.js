892f23e85b18d246f086c434c204d6526e35be4f
luckyrikhari13-cell <luckyrikhari13@gmail.com>
2026-06-23 14:47:23 +0530

    learned something new about scopes in javascript

--- 03_basics/03_scopes.js
+++ 03_basics/03_scopes.js
@@ -1,15 +1,15 @@
 
 //{}=> in javascript these are scopes
 // var c = 500
-let a = 300
-if(true)
-{
+// let a = 300
+// if(true)
+// {
 //these 3 variable should have their scope inside this {} so that it create suitablitilty for programmers
-let a = 10
-const b = 20
+// let a = 10
+// const b = 20
 // var c = 500//=> this is the root cause of the problem
-}
-console.log(a)
+// }
+// console.log(a)
 // console.log(b)
 // console.log(c)
 
@@ -24,3 +24,47 @@ console.log(a)
 // const:
 // Block-scoped like let.
 // Cannot be redeclared or reassigned after initialization, but object/array contents can still be modified.
+
+// in the function inside function scope the inner function or the child function can access the variables of the parent function
+function one(){
+    const username = "lucky"
+    function two(){
+        const website = "youtube"
+        // console.log(username)
+    }
+    // console.log(website);
+    two()
+}
+
+// one() 
+
+if(true){
+    const username = "hitesh"
+    if(username === "hitesh"){ 
+        const website = " youtube"
+        // console.log(username+website)
+    }
+    // console.log(website)
+}
+
+// console.log(username)
+
+
+//++++++++++++++++++++ interesting example ++++++++++++++++++++
+
+//below one is a function
+console.log(addone(5)) // yeah we can do that we can call the function above where it is declared
+function addone(num){
+    return num+1;
+}
+
+// below one is a expression
+// we will call add two pass it a value and that value will be passed to the function and the result will be return to the addTwo
+
+
+// but if we have defined the function inside a variable and calling that variable above that variable it will not work 
+// because javascript will think that the function/ variable we are calling has not been declared yet
+addTwo(5)
+const addTwo = function(num){
+    return num+2;
+}
