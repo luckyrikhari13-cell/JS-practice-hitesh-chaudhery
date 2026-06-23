1c4d5ec9ba2c7105fe7472e68b4a546ea7c59e0c
luckyrikhari13-cell <luckyrikhari13@gmail.com>
2026-06-23 06:26:38 +0530

    understand about the scopes of the let , var and const

---
+++ 03_basics/03_scopes.js
@@ -0,0 +1,24 @@
+// var c = 500
+let a = 300
+if(true)
+{
+//these 3 variable should have their scope inside this {} so that it create suitablitilty for programmers
+let a = 10
+const b = 20
+// var c = 500//=> this is the root cause of the problem
+}
+console.log(a)
+// console.log(b)
+// console.log(c)
+
+// var:
+// Function-scoped (or globally scoped if declared outside a function).
+// Can be redeclared and updated. Not block-scoped, so it ignores {} blocks.
+
+// let:
+// Block-scoped, meaning it only exists inside the {} where it is declared.
+// Can be updated but cannot be redeclared in the same scope.
+
+// const:
+// Block-scoped like let.
+// Cannot be redeclared or reassigned after initialization, but object/array contents can still be modified.
