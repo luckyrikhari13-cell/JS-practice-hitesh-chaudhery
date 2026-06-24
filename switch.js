f2c835978585858cb293930289cb01f77568998f
luckyrikhari13-cell <luckyrikhari13@gmail.com>
2026-06-24 15:27:54 +0530

    learned about switches in javascript

---
+++ switch.js
@@ -0,0 +1,53 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "march"
+switch(month){
+    case 1 :
+        console.log("january")
+        break;
+    case 2 :
+        console.log("feb")
+        break;
+    case 3 :
+        console.log("march")
+        break;
+    case  4:
+        console.log("ap")
+        break;
+    case  5:
+        console.log("ju")
+        break;
+    case  6:
+        console.log("jul")
+        break;
+    case  7:
+        console.log("aug")
+        break;
+    case  8:
+        console.log("sept")
+        break;
+    case  9:
+        console.log("oct")
+        break;
+    case  10:
+        console.log("non")
+        break;
+    case  11:
+        console.log("non")
+        break;
+    case 12 :
+        console.log("dec")
+        break;
+    default :
+        console.log("Default case match")
+        break;
+}
+
+// imp for interview not putting break will execute all the statement below it except break
